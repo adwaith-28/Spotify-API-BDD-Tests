c0c1df83680fb8903f5417b78bb8d470e4b8ef25
Adwaith Jayasankar <adwaith.jayasankar@soti.net>
2025-07-03 15:40:42 +0530

    Update: Secured client id and client secret

---
+++ Helpers/ConfigHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Reflection;
+
+namespace SpotifyApi.BDDTests.Helpers
+{
+    public class ConfigHelper
+    {
+        private static IConfigurationRoot config;
+
+        static ConfigHelper()
+        {
+            config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddUserSecrets(Assembly.GetExecutingAssembly())
+                .Build();
+        }
+
+        public static string GetSpotifyClientId() => config["Spotfy:ClientId"];
+
+        public static string GetSpotifyClientSecret() => config["Spotify:ClientSecret"];
+    }
+}
